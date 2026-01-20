5e055074baf92a1c667881a603f8b2baec1724fb
ahmedabrar914 <56966481+ahmedabrar914@users.noreply.github.com>
2026-01-20 19:29:15 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -25,3 +25,11 @@ variable "zotel_ingest_token" {
   type      = string
   sensitive = true
 }
+
+# --- zotel / otel agent ---
+zotel_install_dir      = var.zotel_install_dir
+zotel_installer_s3_uri = var.zotel_installer_s3_uri
+zotel_installer_name   = var.zotel_installer_name
+zotel_service_group    = var.zotel_service_group
+zotel_ingest_host      = var.zotel_ingest_host
+zotel_ingest_token     = var.zotel_ingest_token
