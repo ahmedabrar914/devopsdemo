1285366c2b357e0f18215c3b812066e26c4d4a76
ahmedabrar914 <56966481+ahmedabrar914@users.noreply.github.com>
2026-01-20 19:17:02 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,24 +1,27 @@
-variable "zotel_installer_s3" {
+variable "zotel_install_dir" {
+  type    = string
+  default = "/sc"
+}
+
+variable "zotel_installer_s3_uri" {
   type = string
-  default = "s3://vaas-prod-artifacts/otel-artifacts/zotelagent_1.6.0_p2_b3278_rhel.sh"
 }
 
 variable "zotel_installer_name" {
-  type = string
+  type    = string
   default = "zotelagent_1.6.0_p2_b3278_rhel.sh"
 }
 
 variable "zotel_service_group" {
-  type = string
+  type    = string
   default = "vault_as_a_service"
 }
 
 variable "zotel_ingest_host" {
   type = string
-  default = "observability2-dev-ingest.zscaler.com"
 }
 
 variable "zotel_ingest_token" {
-  type = string
+  type      = string
   sensitive = true
 }
