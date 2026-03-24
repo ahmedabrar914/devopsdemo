88bf76aa6b8b57afb03b6014540cf047534f326f
ahmedabrar914 <56966481+ahmedabrar914@users.noreply.github.com>
2026-03-24 12:45:16 +0530

    Create main.tf

---
+++ module/batch_token/main.tf
@@ -0,0 +1,173 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    archive = {
+      source = "hashicorp/archive"
+    }
+  }
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_src/lambda_function.py"
+  output_path = "${path.module}/lambda_src/lambda_function.zip"
+}
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = "${var.function_name}-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid    = "ReadInputSecrets"
+    effect = "Allow"
+
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret"
+    ]
+
+    resources = [
+      var.vault_ca_secret_id,
+      var.vault_root_token_secret_id
+    ]
+  }
+
+  statement {
+    sid    = "WriteRotatedSecret"
+    effect = "Allow"
+
+    actions = [
+      "secretsmanager:PutSecretValue",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:UpdateSecret"
+    ]
+
+    resources = [
+      var.rotated_token_secret_id
+    ]
+  }
+
+  statement {
+    sid    = "VpcAccess"
+    effect = "Allow"
+
+    actions = [
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface",
+      "ec2:AssignPrivateIpAddresses",
+      "ec2:UnassignPrivateIpAddresses",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeVpcs"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "CloudWatchLogs"
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "this" {
+  name   = "${var.function_name}-policy"
+  policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.this.arn
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = var.log_retention_in_days
+}
+
+resource "aws_lambda_function" "this" {
+  function_name = var.function_name
+  role          = aws_iam_role.this.arn
+  runtime       = var.runtime
+  handler       = "lambda_function.lambda_handler"
+  timeout       = var.timeout
+  memory_size   = var.memory_size
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = var.security_group_ids
+  }
+
+  environment {
+    variables = {
+      LOG_LEVEL                    = var.log_level
+      BATCH_TOKEN_TTL              = var.batch_token_ttl
+      ROTATED_TOKEN_SECRET_ID      = var.rotated_token_secret_id
+      VAULT_CA_SECRET_ID           = var.vault_ca_secret_id
+      VAULT_PRIMARY_ADDR           = var.vault_primary_addr
+      VAULT_ROOT_TOKEN_JSON_KEY    = var.vault_root_token_json_key
+      VAULT_ROOT_TOKEN_SECRET_ID   = var.vault_root_token_secret_id
+      VAULT_CA_SECRET_JSON_KEY     = var.vault_ca_secret_json_key
+      POLICY_NAME                  = var.policy_name
+      ROLE_NAME                    = var.role_name
+      HTTP_TIMEOUT_SECONDS         = var.http_timeout_seconds
+      HTTP_CONNECT_TIMEOUT_SECONDS = var.http_connect_timeout_seconds
+      HTTP_MAX_RETRIES             = var.http_max_retries
+    }
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.this,
+    aws_cloudwatch_log_group.this
+  ]
+}
+
+resource "aws_cloudwatch_event_rule" "this" {
+  count               = var.schedule_expression != null ? 1 : 0
+  name                = "${var.function_name}-schedule"
+  schedule_expression = var.schedule_expression
+}
+
+resource "aws_cloudwatch_event_target" "this" {
+  count = var.schedule_expression != null ? 1 : 0
+  rule  = aws_cloudwatch_event_rule.this[0].name
+  arn   = aws_lambda_function.this.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  count         = var.schedule_expression != null ? 1 : 0
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.this[0].arn
+}
