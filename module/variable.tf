dcbe4a914e05f465ce5a6e535bcd3a34bcfd99c3
ahmedabrar914 <56966481+ahmedabrar914@users.noreply.github.com>
2026-03-24 13:33:55 +0530

    Create variable.tf

---
+++ module/variable.tf
@@ -0,0 +1,92 @@
+variable "enable_vault_batch_token_rotator" {
+  description = "Enable creation of Vault DR batch token rotator lambda"
+  type        = bool
+  default     = false
+}
+
+variable "vault_batch_token_rotator_function_name" {
+  type    = string
+  default = "vault-dr-batch-token-rotator"
+}
+
+variable "vault_batch_token_rotator_runtime" {
+  type    = string
+  default = "python3.12"
+}
+
+variable "vault_batch_token_rotator_timeout" {
+  type    = number
+  default = 60
+}
+
+variable "vault_batch_token_rotator_memory_size" {
+  type    = number
+  default = 256
+}
+
+variable "vault_batch_token_rotator_log_level" {
+  type    = string
+  default = "INFO"
+}
+
+variable "vault_batch_token_rotator_log_retention_in_days" {
+  type    = number
+  default = 14
+}
+
+variable "vault_batch_token_ttl" {
+  type    = string
+  default = "71h"
+}
+
+variable "vault_batch_token_policy_name" {
+  type    = string
+  default = "dr-secondary-promotion"
+}
+
+variable "vault_batch_token_role_name" {
+  type    = string
+  default = "failover-handler"
+}
+
+variable "vault_batch_token_schedule_expression" {
+  type    = string
+  default = null
+}
+
+variable "vault_root_token_secret_id" {
+  type = string
+}
+
+variable "rotated_token_secret_id" {
+  type = string
+}
+
+variable "vault_root_token_json_key" {
+  type    = string
+  default = "root_token"
+}
+
+variable "vault_ca_secret_json_key" {
+  type    = string
+  default = ""
+}
+
+variable "vault_batch_token_http_timeout_seconds" {
+  type    = string
+  default = "10"
+}
+
+variable "vault_batch_token_http_connect_timeout_seconds" {
+  type    = string
+  default = "5"
+}
+
+variable "vault_batch_token_http_max_retries" {
+  type    = string
+  default = "3"
+}
+
+variable "vault_batch_token_security_group_ids" {
+  type = list(string)
+}
