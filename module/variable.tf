994f625af170a402f728f27c31af7f8372161ee2
ahmedabrar914 <56966481+ahmedabrar914@users.noreply.github.com>
2026-03-31 14:19:48 +0530

    Update variable.tf

--- module/variable.tf
+++ module/variable.tf
@@ -169,3 +169,86 @@ variable "vault_admin_token_explicit_max_ttl" {
   type    = string
   default = "32d"
 }
+
+##admin
+
+variable "enable_vault_admin_token_rotator" {
+  type    = bool
+  default = false
+}
+
+variable "vault_admin_token_rotator_function_name" {
+  type = string
+}
+
+variable "vault_admin_token_rotator_runtime" {
+  type    = string
+  default = "python3.12"
+}
+
+variable "vault_admin_token_rotator_timeout" {
+  type    = number
+  default = 60
+}
+
+variable "vault_admin_token_rotator_memory_size" {
+  type    = number
+  default = 256
+}
+
+variable "vault_admin_token_rotator_log_level" {
+  type    = string
+  default = "INFO"
+}
+
+variable "vault_admin_token_rotator_log_retention_in_days" {
+  type    = number
+  default = 14
+}
+
+variable "vault_admin_token_ttl" {
+  type    = string
+  default = "32d"
+}
+
+variable "vault_admin_token_policy_name" {
+  type = string
+}
+
+variable "vault_admin_token_role_name" {
+  type = string
+}
+
+variable "vault_admin_token_schedule_expression" {
+  type    = string
+  default = null
+}
+
+variable "admin_rotated_token_secret_id" {
+  type = string
+}
+
+variable "vault_admin_token_http_timeout_seconds" {
+  type    = string
+  default = "10"
+}
+
+variable "vault_admin_token_http_connect_timeout_seconds" {
+  type    = string
+  default = "5"
+}
+
+variable "vault_admin_token_http_max_retries" {
+  type    = string
+  default = "3"
+}
+
+variable "vault_admin_token_renewable" {
+  type    = string
+  default = "true"
+}
+
+variable "vault_admin_token_explicit_max_ttl" {
+  type    = string
+  default = "32d"
+}
